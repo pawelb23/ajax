0d36d9f4db5fbae913b1269db95203464f4966dc
Paweł Białek <pawelbialek23@op.pl>
2018-03-03 10:48:00 +0100

    komit

--- 2-ajax-jquery/js/maisn.js
+++ 2-ajax-jquery/js/maisn.js
@@ -2,5 +2,5 @@
 
 $.getJSON("http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl" ,   function (data) {   
     console.log(data);
-    //console.log(data.userId);// console log z linijki czwartej jest tylko pomocniczy
+    console.log(data.userId);// console logi z linijek czwartej  i piątej są tylko pomocnicze
 });
